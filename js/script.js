da15670d6427bbb719f0b893f14c5fafdbcca22f
sing6709 <ramandeep-singh25@outlook.com>
2020-11-05 23:25:12 +0100

    when logo clicked it will take you to the top of the page, navbar goes solid upon scroll

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Transform navbar from transparent to solid on scroll
+var nav = document.getElementById("navbar");
+
+window.onscroll = function(){
+    if(window.pageYOffset > 300){
+        nav.style.background = "#1C1C1C";
+    }
+    else{
+        nav.style.background = "rgba(0,0,0,0.2)";
+    }
+}
